111505d1509b3b6677b2fa301d38fd4bc209572a
piedhorse <umutalacatli@icloud.com>
2023-03-14 02:03:56 +0300

    Add project files.

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Authmvc.Models;
+using System.Linq;
+
+namespace Authmvc.Controllers
+{
+    public class AccountsController : Controller
+    {   
+        registeredusersEntities entity = new registeredusersEntities();
+
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel credenctials)
+        {
+            bool userExist = entity.UsersTbls.Any(x =>x.Email == credenctials.Email && x.Passcode == credenctials.Password );
+            UsersTbl u = entity.UsersTbls.FirstOrDefault(x => x.Email == credenctials.Email && x.Passcode == credenctials.Password);
+            if (userExist) {
+                FormsAuthentication.SetAuthCookie(u.Username, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError("", "Username or password is wrong");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(UsersTbl userinfo)
+        {
+            entity.UsersTbls.Add(userinfo);
+            entity.SaveChanges();
+
+            return RedirectToAction("Login");
+        } 
+
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Authmvc.MvcApplication" Language="C#" %>
